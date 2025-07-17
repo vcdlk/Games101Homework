03f5fdd6fb7a7605ace2e65bbbdacb9cc4643907
vcdlk <476041181@qq.com>
2025-07-17 11:46:02 +0800

    feat(init): main

--- Homework3/Assignment3/main.cpp
+++ Homework3/Assignment3/main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
@@ -266,70 +267,64 @@ Eigen::Vector3f displacement_fragment_shader(const fragment_shader_payload& payl
 
 Eigen::Vector3f bump_fragment_shader(const fragment_shader_payload& payload)
 {
-  Eigen::Vector3f ka = Eigen::Vector3f(0.005, 0.005, 0.005);
-  Eigen::Vector3f kd = payload.color;
-  Eigen::Vector3f ks = Eigen::Vector3f(0.7937, 0.7937, 0.7937);
+    Eigen::Vector3f ka = Eigen::Vector3f(0.005, 0.005, 0.005);
+    Eigen::Vector3f kd = payload.color;
+    Eigen::Vector3f ks = Eigen::Vector3f(0.7937, 0.7937, 0.7937);
 
-  auto l1 = light{{20, 20, 20}, {500, 500, 500}};
-  auto l2 = light{{-20, 20, 0}, {500, 500, 500}};
+    auto l1 = light{{20, 20, 20}, {500, 500, 500}};
+    auto l2 = light{{-20, 20, 0}, {500, 500, 500}};
 
-  std::vector<light> lights = {l1, l2};
-  Eigen::Vector3f amb_light_intensity{10, 10, 10};
-  Eigen::Vector3f eye_pos{0, 0, 10};
+    std::vector<light> lights = {l1, l2};
+    Eigen::Vector3f amb_light_intensity{10, 10, 10};
+    Eigen::Vector3f eye_pos{0, 0, 10};
 
-  float p = 150;
+    float p = 150;
 
-  Eigen::Vector3f color = payload.color;
-  Eigen::Vector3f point = payload.view_pos;
-  Eigen::Vector3f normal = payload.normal;
+    Eigen::Vector3f color = payload.color;
+    Eigen::Vector3f point = payload.view_pos;
+    Eigen::Vector3f normal = payload.normal;
 
-  float kh = 0.2, kn = 0.1;
+    float kh = 0.2, kn = 0.1;
 
-  // TODO: Implement bump mapping here
-  // Let n = normal = (x, y, z)
-  // Vector t = (x*y/sqrt(x*x+z*z),sqrt(x*x+z*z),z*y/sqrt(x*x+z*z))
-  // Vector b = n cross product t
-  // Matrix TBN = [t b n]
-  // dU = kh * kn * (h(u+1/w,v)-h(u,v))
-  // dV = kh * kn * (h(u,v+1/h)-h(u,v))
-  // Vector ln = (-dU, -dV, 1)
-  // Normal n = normalize(TBN * ln)
+    // TODO: Implement bump mapping here
+    // Let n = normal = (x, y, z)
+    // Vector t = (x*y/sqrt(x*x+z*z),sqrt(x*x+z*z),z*y/sqrt(x*x+z*z))
+    // Vector b = n cross product t
+    // Matrix TBN = [t b n]
+    // dU = kh * kn * (h(u+1/w,v)-h(u,v))
+    // dV = kh * kn * (h(u,v+1/h)-h(u,v))
+    // Vector ln = (-dU, -dV, 1)
+    // Normal n = normalize(TBN * ln)
 
-  float x = normal.x();
-  float y = normal.y();
-  float z = normal.z();
-  // Eigen::Vector3f t = Eigen::Vector3f(x*y / sqrt(x*x+z*z), sqrt(x*x+z*z),
-  // z*y/sqrt(x*x+z*z));
-  Eigen::Vector3f t =
-      Eigen::Vector3f(-x * z / sqrt(x * x + y * y),
-                      -y * z / sqrt(x * x + y * y), 1 / sqrt(x * x + y * y));
-  Eigen::Vector3f b = normal.cross(t);
-  Eigen::Vector3f n = normal;
+    float x = normal.x();
+    float y = normal.y();
+    float z = normal.z();
 
-  Matrix3f TBN;
-  TBN.col(0) = t;
-  TBN.col(1) = b;
-  TBN.col(2) = n;
-  // std::cout << TBN << std::endl;
+    // Eigen::Vector3f t = Eigen::Vector3f(x * y / std::sqrt(x * x + z * z), std::sqrt(x * x + z * z), z * y / std::sqrt(x * x + z * z));
+    Eigen::Vector3f t = Eigen::Vector3f(-y / std::sqrt(x * x + y * y), x / sqrt(x * x + y * y), 0);
 
-  float u = payload.tex_coords.x();
-  float v = payload.tex_coords.y();
+    Eigen::Vector3f b = normal.cross(t);
 
-  float w = payload.texture->width;
-  float h = payload.texture->height;
+    Eigen::Matrix3f TBN;
+    TBN << t.x(), b.x(), normal.x(),
+        t.y(), b.y(), normal.y(),
+        t.z(), b.z(), normal.z();
 
-  // float a = payload.texture->getColor(std::clamp(u+1/w,
-  // 0.0f, 1.0f),v).norm()-payload.texture->getColor(u,v).norm();
-  float dU = kh * kn * (payload.texture->getColor((u + 1) / w, v).norm() - payload.texture->getColor(u, v).norm());
-  float dV = kh * kn * (payload.texture->getColor(u, (v + 1) / h).norm() - payload.texture->getColor(u, v).norm());
-  auto ln = Eigen::Vector3f(-dU, -dV, 1);
+    float u = payload.tex_coords.x();
+    float v = payload.tex_coords.y();
+    float w = payload.texture->width;
+    float h = payload.texture->height;
 
-  Eigen::Vector3f result_color = TBN * ln;
+    float dU = kh * kn * (payload.texture->getColor(u + 1.0f / w, v).norm() - payload.texture->getColor(u, v).norm());
+    float dV = kh * kn * (payload.texture->getColor(u, v + 1.0f / h).norm() - payload.texture->getColor(u, v).norm());
 
-  // Eigen::Vector3f result_color = {0, 0, 0};
-  // result_color = normal;
+    Eigen::Vector3f ln = Eigen::Vector3f(-dU, -dV, 1.0f);
+    normal = TBN * ln;
 
-  return result_color * 255.f;
+    Eigen::Vector3f result_color = {0, 0, 0};
+    result_color = normal.normalized();
+
+    return result_color * 255.f;
 }
 
 int main(int argc, const char** argv)
